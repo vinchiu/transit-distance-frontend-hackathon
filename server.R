eb4dba5fb75762b242eb440f7d4a2e2563cbb9a5
chiu <tradeaardax@gmail.com>
2016-03-05 19:34:49 -0800

    kinda works

--- server.R
+++ server.R
@@ -4,7 +4,7 @@ library(shiny)
 shinyServer(function(input, output) {
   output$phonePlot <- renderPlot({
     # Render a barplot
-    LoadTransitData(input$price_input)
+  LoadTransitData(input$price_input)
   })
   # Reactive expression to compose a data frame containing all of
   # the values
@@ -12,13 +12,13 @@ shinyServer(function(input, output) {
     # Compose data frame
     data.frame(
       Name = c("price_input"),
-      Value = as.character(c(input$decimal)),
+      Value = as.character(c(input$price_input)),
       stringsAsFactors = FALSE
     )
   })
   
   # Show the values using an HTML table
-  output$values <- renderTable({
-    sliderValues()
-  })
+#   output$values <- renderTable({
+#     sliderValues()
+#   })
 })
